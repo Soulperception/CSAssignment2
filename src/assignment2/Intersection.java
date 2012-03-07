ebb9044886c9ab38c9dd55bbea964c682cf114e0
Joe Chua <joechuajl@gmail.com>
2012-03-07 15:04:41 +0800

    Coded placeholder applet for Intersection

--- src/assignment2/Intersection.java
+++ src/assignment2/Intersection.java
@@ -1,6 +1,14 @@
 package assignment2;
 
-public class Intersection
+import javax.swing.JLabel;
+import javax.swing.JApplet;
+
+public class Intersection extends JApplet
 {
-	
+	private static final long serialVersionUID = 1L;
+
+	public void init()
+	{
+		add(new JLabel("test"));
+	}
 }
